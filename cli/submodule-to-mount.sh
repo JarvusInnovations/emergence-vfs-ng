fc1180ca328978becb2d226d8e59bd654192b257
Chris Alfano <chris@jarv.us>
2016-02-25 13:56:33 -0400

    Fix hardcoded package name in submodule convert script

--- cli/submodule-to-mount.sh
+++ cli/submodule-to-mount.sh
@@ -6,5 +6,5 @@ echo "Converting $PACKAGE"
 
 git rm -r sencha-workspace/packages/$PACKAGE
 echo -e "[source]\n    url = https://github.com/JarvusInnovations/$PACKAGE.git\n    branch = master" > .gitsources/$PACKAGE
-echo -e "[mount]\n    sourcepath=/\n    mountpath=jarvus-apikit" > sencha-workspace/packages/.gitmounts/$PACKAGE
+echo -e "[mount]\n    sourcepath=/\n    mountpath=$PACKAGE" > sencha-workspace/packages/.gitmounts/$PACKAGE
 git add .gitsources sencha-workspace/packages/.gitmounts
