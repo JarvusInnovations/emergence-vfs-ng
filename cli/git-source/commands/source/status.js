468c0f17bc90fb5ce00c818a536f8d50577f73a4
Chris Alfano <chris@jarv.us>
2016-02-25 15:14:51 -0400

    Implement 'git source status' command to show status of sources

---
+++ cli/git-source/commands/source/status.js
@@ -0,0 +1,87 @@
+var app = require('../../source'),
+    lib = require('../../lib'),
+    fs = require('fs'),
+    path = require('path'),
+    async = require('async'),
+    Table = require('easy-table');
+
+module.exports = function(callback) {
+    lib.getSources(function(error, sources) {
+        if (error) {
+            app.log.error('Failed got get sources', error.message);
+            return callback(error);
+        }
+
+        async.each(sources, function(source, callback) {
+            async.auto({
+                initialized: function(callback) {
+                    if ('initialized' in source) {
+                        return callback();
+                    }
+
+                    fs.exists(source.gitDir, function(exists) {
+                        source.initialized = exists;
+                        callback();
+                    });
+                },
+
+                shallow: [
+                    'initialized',
+                    function(callback) {
+                        if ('shallow' in source) {
+                            return callback();
+                        }
+
+                        if (!source.initialized) {
+                            source.shallow = null;
+                            return callback();
+                        }
+
+                        fs.readFile(path.join(source.gitDir, 'shallow'), 'utf8', function(error, shallow) {
+                            if (error && error.code != 'ENOENT') {
+                                return callback(error);
+                            }
+
+                            source.shallow = shallow ? shallow.trim() : false;
+                            callback();
+                        });
+                    }
+                ],
+
+                shallowTag: [
+                    'shallow',
+                    function(callback, results) {
+                        if ('shallowTag' in source) {
+                            return callback();
+                        }
+
+                        if (!source.shallow) {
+                            source.shallowTag = null;
+                            return callback();
+                        }
+
+                        source.execGit('describe', { tags: true }, source.shallow, function(error, output) {
+                            source.shallowTag = output || false;
+                            callback();
+                        });
+                    }
+                ]
+            }, callback);
+        }, function(error) {
+            if (error) {
+                app.log.error('Failed to check source status', error.message);
+                return callback(error);
+            }
+
+            console.log(Table.print(sources.map(function(source) {
+                return {
+                    name: source.name,
+                    initialized: source.initialized ? 'yes' : 'no',
+                    shallow: source.shallow === null ? '' : source.shallow === false ? '\033[31mno\033[0m' : source.shallowTag ? '\033[32m' + source.shallowTag + '\033[0m' : source.shallow.substr(0, 6)
+                };
+            })));
+
+            callback();
+        });
+    });
+};
